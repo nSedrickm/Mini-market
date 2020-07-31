9d5eb0556e2e7c9a589dd3e5e388f983baf7867b
nSedrickm <nsedrick101@gmail.com>
2020-07-31 22:50:46 +0100

    error now look nice

--- app.js
+++ app.js
@@ -36,7 +36,10 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    layout: false,
+    title: err.status + " " + err.message
+  });
 });
 
 module.exports = app;
