1002dd8ea597056c62df81f5967ec02762e01841
nSedrickm <nsedrick101@gmail.com>
2020-08-13 00:13:06 +0100

    export sorta works for reports

--- public/assets/js/data-tables.js
+++ public/assets/js/data-tables.js
@@ -4,4 +4,42 @@ $(document).ready(function () {
     $('#basic-data-table').DataTable({
         "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
     });
+
+    var buttonCommon = {
+        exportOptions: {
+            format: {
+                body: function (data, row, column, node) {
+                    // Strip $ from salary column to make it numeric
+                    return column === 5 ?
+                        data.replace(/[$,]/g, '') :
+                        data;
+                }
+            }
+        }
+    };
+
+    $('#example-report').DataTable({
+        ajax: 'http://localhost:8001/objects.txt',
+        columns: [
+            { data: 'name' },
+            { data: 'position' },
+            { data: 'office' },
+            { data: 'extn' },
+            { data: 'start_date' },
+            { data: 'salary' }
+        ],
+        dom: 'Bfrtip',
+        buttons: [
+            $.extend(true, {}, buttonCommon, {
+                extend: 'copyHtml5'
+            }),
+            $.extend(true, {}, buttonCommon, {
+                extend: 'excelHtml5'
+            }),
+            $.extend(true, {}, buttonCommon, {
+                extend: 'pdfHtml5'
+            })
+        ]
+    });
 });
+
