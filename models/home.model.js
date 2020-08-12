893e9eb38dfe5ae1a2a5d093e8eb4c1777b52412
nSedrickm <nsedrick101@gmail.com>
2020-08-12 23:06:10 +0100

    add query to extract best selling product

--- models/home.model.js
+++ models/home.model.js
@@ -3,7 +3,11 @@ var mysqlConn = require('../config/db.config');
 
 getMetrics = function (result) {
     const sql = `SELECT (SELECT count(*) from products) AS products,
-    (SELECT count(*) from orders)  AS orders`;
+    (SELECT count(*) from orders)  AS orders, (SELECT name
+        FROM orders
+        GROUP BY name
+        ORDER BY COUNT(*) DESC
+        LIMIT 1) AS best_seller`;
 
     mysqlConn.query(sql, function (err, data) {
         if (err) {
