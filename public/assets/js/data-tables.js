4bfbd479d92b396dd164c676a487a3ee102fbb81
nSedrickm <nsedrick101@gmail.com>
2020-08-12 23:41:15 +0100

    add and init datatables

---
+++ public/assets/js/data-tables.js
@@ -0,0 +1,7 @@
+
+//start all data tables 
+$(document).ready(function () {
+    $('#basic-data-table').DataTable({
+        "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+    });
+});
