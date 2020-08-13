072f91fc7cf65a65468442563e82b2a0d9f7156c
nSedrickm <nsedrick101@gmail.com>
2020-08-13 11:40:10 +0100

    orders report works

--- public/assets/js/data-tables.js
+++ public/assets/js/data-tables.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         }
     };
 
-
+    /* products report generation */
     $('#products-report-table').DataTable({
         dom: 'Bfrtip',
         buttons: [
@@ -45,6 +45,38 @@ $(document).ready(function () {
                 autoPrint: false,
                 className: 'btn text-light shadow border-0 bg-primary',
                 title: 'Products Report',
+                messageTop: 'Generated : ' + new Date().toUTCString(),
+            })
+        ]
+    });
+
+    /* orders report generation */
+    $('#orders-report-table').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            $.extend(true, {}, buttonCommon, {
+                extend: 'csvHtml5',
+                className: 'btn text-light shadow border-0 bg-secondary ',
+                title: 'Orders Report',
+                messageTop: 'Generated : ' + new Date().toUTCString(),
+            }),
+            $.extend(true, {}, buttonCommon, {
+                extend: 'excelHtml5',
+                className: 'btn text-light shadow border-0 bg-success',
+                title: 'Orders Report',
+                messageTop: 'Generated : ' + new Date().toUTCString(),
+            }),
+            $.extend(true, {}, buttonCommon, {
+                extend: 'pdfHtml5',
+                className: 'btn text-light shadow border-0 bg-danger',
+                title: 'Orders Report',
+                messageTop: 'Generated : ' + new Date().toUTCString(),
+            }),
+            $.extend(true, {}, buttonCommon, {
+                extend: 'print',
+                autoPrint: false,
+                className: 'btn text-light shadow border-0 bg-primary',
+                title: 'Orders Report',
                 messageTop: 'Generated : ' + new Date().toUTCString(),
             })
         ]
