014355a01c7c3e3810cb4a6efb3865515bec55ef
nSedrickm <nsedrick101@gmail.com>
2020-08-13 12:00:22 +0100

    products.hbs update

--- public/assets/js/data-tables.js
+++ public/assets/js/data-tables.js
@@ -1,9 +1,7 @@
 
 //start all data tables 
 $(document).ready(function () {
-    $('#basic-data-table').DataTable({
-        "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-    });
+    $('#basic-data-table').DataTable();
 
     var buttonCommon = {
         exportOptions: {
