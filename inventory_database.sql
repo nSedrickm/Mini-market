fa2f238ef87c683547e7ba7ec4638f43f505cf7c
nSedrickm <nsedrick101@gmail.com>
2020-08-14 11:00:55 +0100

    update db

--- inventory_database.sql
+++ inventory_database.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 04, 2020 at 10:43 AM
--- Server version: 10.3.22-MariaDB-0+deb10u1
+-- Generation Time: Aug 14, 2020 at 11:00 AM
+-- Server version: 10.3.23-MariaDB-0+deb10u1
 -- PHP Version: 7.3.19-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -44,12 +44,14 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `name`, `category`, `price`, `quantity`, `total`, `date`, `time`) VALUES
-(3, 'Sardine', 'groceries', 500, 10, 5000, '2020-08-12', '10:00:00'),
+(3, 'Sardinezz', 'groceries', 50000, 10, 5000, '2020-08-13', '22:34:00'),
 (4, 'Sardine', 'groceries', 500, 10, 5000, '2020-08-12', '10:00:00'),
 (5, 'Sardine', 'groceries', 500, 10, 5000, '2020-08-12', '10:00:00'),
 (6, 'Toothbrush', 'sanitation', 100, 1, 100, '2020-08-04', '07:08:00'),
 (8, 'Toothbrush', 'sanitation', 100, 1, 100, '2020-08-04', '07:10:00'),
-(9, 'Sardivan', 'as', 50, 5, 250, '2020-08-04', '07:21:00');
+(9, 'Sardivan', 'as', 50, 5, 250, '2020-08-04', '07:21:00'),
+(10, 'Sardine', 'groceries', 500, 10, 5000, '2020-08-13', '22:09:00'),
+(11, 'Milk', 'groceries', 100, 10, 1000, '2020-08-14', '06:34:00');
 
 -- --------------------------------------------------------
 
@@ -78,17 +80,14 @@ INSERT INTO `products` (`id`, `name`, `category`, `price`, `quantity`, `supplier
 (3, 'Milk', 'groceries', 50, 100, 'CAMLAIT', '2020-08-03', '15:25:00'),
 (4, 'Sardine', 'groceries', 50, 100, 'Sardine SARL', '2020-08-03', '15:25:00'),
 (6, 'Sardine', 'groceries', 50, 1000, 'Sardine SARL', '2020-08-03', '15:25:00'),
-(7, 'Maggi', 'groceries', 50, 100, 'MAGGICAM', '2020-08-03', '18:22:00'),
 (8, 'Maggi', 'groceries', 50, 100, 'MAGGICAM', '2020-08-03', '15:25:00'),
 (9, 'Milk', 'groceries', 50, 100, 'CAMLAIT', '2020-08-03', '15:25:00'),
 (10, 'Sardine', 'groceries', 50, 100, 'Sardine SARL', '2020-08-03', '15:25:00'),
 (11, 'Sardine', 'groceries', 50, 1000, 'Sardine SARL', '2020-08-03', '15:25:00'),
 (12, 'Salt', 'seasoning', 100, 7, 'Salt SA', '2020-08-03', '18:38:00'),
-(13, 'Salt', 'seasoning', 100, 7, 'Salt SA', '2020-08-03', '18:38:00'),
 (16, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:42:00'),
 (17, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:51:00'),
 (18, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:51:00'),
-(19, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:51:00'),
 (20, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:51:00'),
 (21, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:51:00'),
 (22, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '18:51:00'),
@@ -105,13 +104,13 @@ INSERT INTO `products` (`id`, `name`, `category`, `price`, `quantity`, `supplier
 (34, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '19:41:00'),
 (35, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '19:41:00'),
 (37, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '19:47:00'),
-(38, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '20:05:00'),
 (39, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '20:05:00'),
 (40, 'Salt', 'seasoning', 100, 100, 'Salt SA', '2020-08-03', '20:05:00'),
 (41, 'Sardine', 'groceries', 61, 34, 'SardineSA', '2020-08-03', '20:53:00'),
 (42, 'fish ', 'fish ', 500, 10, 'Cold house', '2020-08-03', '20:56:00'),
 (43, 'fish ', 'fish ', 500, 10, 'Cold house', '2020-08-03', '21:00:00'),
-(45, 'chocolate', 'candy', 500, 5, 'CHOCOCAM', '2020-08-03', '21:02:00');
+(45, 'Chocolate', 'candy too', 500, 18, 'CHOCOCAM', '2020-08-13', '22:28:00'),
+(46, 'Oil', 'groceries', 500, 10, 'OILCAM', '2020-08-14', '06:35:00');
 
 -- --------------------------------------------------------
 
@@ -122,11 +121,22 @@ INSERT INTO `products` (`id`, `name`, `category`, `price`, `quantity`, `supplier
 CREATE TABLE `suppliers` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `phone_number` int(255) NOT NULL,
-  `product_name` int(11) NOT NULL,
-  `category` int(11) NOT NULL,
-  `price` int(100) NOT NULL
+  `phone_number` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `price` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `name`, `phone_number`, `product_name`, `category`, `price`) VALUES
+(1, 'CAMFISH', '23764589756', 'Fish', 'groceries', 500000),
+(3, 'BONBON', '234566789', 'bonbon', 'sweets', 50),
+(4, 'BONBON', '234566789', 'bonbon', 'sweets', 50),
+(5, 'BONBON', '234566789', 'bonbon', 'sweets', 50),
+(6, 'BONBON', '234566789', 'bonbon', 'sweets', 50);
 
 -- --------------------------------------------------------
 
@@ -145,7 +155,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `password`) VALUES
-(1, 'admin', 'admin');
+(1, 'admin', 'admin'),
+(2, 'admin2', 'admin2');
 
 --
 -- Indexes for dumped tables
@@ -183,25 +194,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
