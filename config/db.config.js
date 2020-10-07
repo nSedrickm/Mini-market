97dccdbe44ebfad633e94e33b441c6fd374f15a7
nSedrickm <nsedrick101@gmail.com>
2020-10-07 15:59:02 +0100

    use pooling to stop disconnect with cleardb

--- config/db.config.js
+++ config/db.config.js
@@ -3,14 +3,14 @@
 const mysql = require('mysql');
 
 //local mysql db connection
-const mysqlConn = mysql.createConnection({
+const mysqlConn = mysql.createPool({
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'b88dcdf579b79e',
     password: 'dac4546c',
     database: 'heroku_3fb3a0c1a7020c0'
 });
-mysqlConn.connect(function (err) {
-    if (err) throw err;
+pool.on('acquire', function (connection) {
+    console.log('Connection %d acquired', connection.threadId);
     console.log("Database Connected!");
 });
 module.exports = mysqlConn;
